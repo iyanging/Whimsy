3e865336251a384a665473b7c6af4b166bc71bba
iyanging <iyanging@163.com>
2025-03-11 10:46:08 +0800

    feat(fgtc): handle exception

--- from-generator-to-coroutine/gen_co.py
+++ from-generator-to-coroutine/gen_co.py
@@ -1,153 +1,191 @@
 from collections.abc import Callable, Generator
 from functools import wraps
-from time import time
 from types import GeneratorType
 from typing import Any, cast
 
 
-class _Missing: ...
+class Task[T]:
+    """对连续运行程序片段的抽象, Continuation 的外在表现, Event Loop 调度的基本对象, 结果的容器.
 
+    参考: https://en.wikipedia.org/wiki/Continuation
+    """
 
-class Future[T]:
-    """一次性的、用于传递结果数据的，容器."""
-
-    _parent: "Future[Any] | None"
-    _generator: Generator["Future[Any] | None", Any, Any] | None
-    _result: T | _Missing
+    _parent: "Task[Any] | None"
+    _generator: Generator["Task[Any] | None", Any, Any] | None
+    _result: "T | _Missing"
+    _exception: BaseException | None
 
     def __init__(self) -> None:
         self._parent = None
         self._generator = None
         self._result = _Missing()
+        self._exception = None
 
     def x_set_generator(
-        self, generator: Generator["Future[Any] | None", Any, Any]
+        self,
+        generator: Generator["Task[Any] | None", Any, Any],
     ) -> None:
         self._generator = generator
 
-    def x_generator(self) -> Generator["Future[Any] | None", Any, Any]:
+    def x_generator(self) -> Generator["Task[Any] | None", Any, Any]:
         if self._generator is None:
             raise Exception("generator should not be None")
 
         return self._generator
 
-    def x_set_parent(self, parent: "Future[Any]") -> None:
+    def x_set_parent(self, parent: "Task[Any]") -> None:
         self._parent = parent
 
-    def x_parent(self) -> "Future[Any] | None":
+    def x_parent(self) -> "Task[Any] | None":
         return self._parent
 
     def set_result(self, result: T) -> None:
-        if not self.done():
+        if isinstance(self._result, _Missing) and self._exception is None:
             self._result = result
+
         else:
-            raise Exception("Future already had result")
+            raise InvalidStateError()
+
+    def set_exception(self, exception: BaseException) -> None:
+        if isinstance(self._result, _Missing) and self._exception is None:
+            self._exception = exception
+
+        else:
+            raise InvalidStateError()
 
     def result(self) -> T:
-        if self.done():
-            return cast(T, self._result)
+        if not isinstance(self._result, _Missing):
+            return self._result
+
+        elif self._exception is not None:
+            raise self._exception
+
         else:
-            raise Exception("Future has no result")
+            raise InvalidStateError()
 
     def done(self) -> bool:
-        return not isinstance(self._result, _Missing)
+        return (not isinstance(self._result, _Missing)) or self._exception is not None
 
     def __repr__(self) -> str:
-        gen = cast(GeneratorType[Future[Any], Any, None] | None, self._generator)
+        gen = cast(GeneratorType[Task[Any], Any, None] | None, self._generator)
         gen_qualname = gen.gi_code.co_qualname + "()" if gen is not None else None
 
-        return f"<{self.__class__.__qualname__}, gen: {gen_qualname}, parent: {repr(self._parent)}>"
+        return (
+            f"<{self.__class__.__qualname__}, "
+            f"gen: {gen_qualname}, "
+            f"parent: {repr(self._parent)}>"
+        )
 
 
 def awaitable[**P, R](
     #
-    # 怎么理解 Generator[Future[Any] | None, Any, R] ?
+    # 怎么理解 Generator[Task[Any] | None, Any, R] ?
     #
     #     对于1个模拟协程, 内部在使用 yield 时, 大概有两种目的:
     #
-    #       1. Future[Any], Any, R -> 调用其他模拟协程:
-    #           会把被调用的模拟协程创建的 Future[?] 给返出来,
-    #           并且希望恢复执行时, 能在此断点处取得上该 Future[?] 里的结果
+    #       1. Task[Any], Any, R -> 调用其他模拟协程:
+    #           会把被调用的模拟协程创建的 Task[?] 给返出来,
+    #           并且希望恢复执行时, 能在此断点处取得上该 Task[?] 里的结果
     #
     #       2. None, None, R -> 当前模拟协程等待操作完成, 主动归还执行流
     #
-    func: Callable[P, Generator[Future[Any] | None, Any, R]],
-) -> Callable[P, Future[R]]:
-    """将 Generator 构造函数转换为 Future 构造函数.
+    func: Callable[P, Generator[Task[Any] | None, Any, R]],
+) -> Callable[P, Task[R]]:
+    """将 Generator 构造函数转换为 Task 构造函数.
 
-    ...使得逻辑可以直接写在 Generator 构造函数中, 避免需要封装到 Future 里.
+    ...使得逻辑可以直接写在 Generator 构造函数中, 避免需要封装到 Task 里.
     """
 
     @wraps(func)
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Future[R]:
-        future = get_running_loop().create_future()
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Task[R]:
+        task = get_running_loop().create_task()
 
         gen = func(*args, **kwargs)
-        future.x_set_generator(gen)
+        task.x_set_generator(gen)
 
-        return future
+        return task
 
     return wrapper
 
 
 class EventLoop:
-    """事件循环, 实现了对 Future 的调度."""
+    """事件循环, 实现了对 Task 的调度."""
 
-    _futures_queue: list[Future[Any]]
-    """Future 队列, 包含正在运行的, 以及待运行的 Future."""
+    _tasks_queue: list[Task[Any]]
+    """Task 队列, 包含正在运行的, 以及待运行的 Task."""
 
     def __init__(self) -> None:
-        self._futures_queue = []
+        self._tasks_queue = []
 
-    def create_future(self) -> Future[Any]:
-        return Future[Any]()
+    def create_task(self) -> Task[Any]:
+        return Task[Any]()
 
-    def run_until_complete[T](self, future: Future[T]) -> T:
-        self._futures_queue.append(future)
+    def run_until_complete[T](self, task: Task[T]) -> T:
+        self._tasks_queue.append(task)
 
-        while len(self._futures_queue) > 0:
-            next_future = self._futures_queue.pop(0)
-            parent_future = next_future.x_parent()
+        # loop 本体: 以某种判定条件, 取出下一个需要运行的 Task, 运行逻辑或返回结果
+        while len(self._tasks_queue) > 0:
+            # 某种判定条件: 直接取队列头部
+            next_task = self._tasks_queue.pop(0)
+            parent_task = next_task.x_parent()
 
-            if not next_future.done():  # future 未完成, 继续向下执行
-                running_future = next_future
+            if not next_task.done():  # task 未完成, 继续向下执行
+                running_task = next_task
                 sending = None
+                throwing = None
 
-            elif parent_future is not None:  # future 已完成, 返回向上执行
-                running_future = parent_future
-                sending = next_future.result()
+            elif parent_task is not None:  # task 已完成, 返回向上执行
+                running_task = parent_task
+                try:
+                    sending = next_task.result()
+                    throwing = None
+                except BaseException as exc:
+                    sending = None
+                    throwing = exc
 
             else:
                 continue
 
-            # 已经抉择了待运行的 future, 删除临时变量避免后续代码误用
-            del next_future
-            del parent_future
+            # 已经抉择了待运行的 task, 删除临时变量避免后续代码误用
+            del next_task
+            del parent_task
 
             # 运行 generator
             try:
-                new_future = running_future.x_generator().send(sending)
+                running_gen = running_task.x_generator()
+
+                if throwing is None:
+                    new_task = running_gen.send(sending)
+
+                else:
+                    new_task = running_gen.throw(throwing)
 
             except StopIteration as stop:  # generator 运行完成, 取得结果
                 result = stop.value
-                running_future.set_result(result)
+                running_task.set_result(result)
 
-                # 将已经完成的 future 送回队列, 结果的返回由后续的循环来操作
-                self._futures_queue.append(running_future)
+                # 将已经完成的 task 送回队列, 结果的返回由后续的循环来操作
+                self._tasks_queue.append(running_task)
 
-            else:  # generator 主动中断, 可能是自身依旧等待完成, 或触发了新的 future
-                if new_future is not None:  # generator 触发了新的 future
-                    new_future.x_set_parent(running_future)
-                    # 控制流转交给新的 Future, 若新的 Future 需要返回值则会创建 resume generator,
-                    # 而无需将老的 Future 再次加入队列
-                    self._futures_queue.append(new_future)
+            except BaseException as exc:  # generator 发生了异常
+                running_task.set_exception(exc)
+
+                # 将已经完成的 task 送回队列, 异常的抛出由后续的循环来操作
+                self._tasks_queue.append(running_task)
+
+            else:  # generator 主动中断, 可能是自身依旧等待完成, 或触发了新的 task
+                if new_task is not None:  # generator 触发了新的 task
+                    new_task.x_set_parent(running_task)
+                    # 控制流转交给新的 Task, 而无需将老的 Task 再次加入队列
+                    # 待新的 Task 取得结果时, 从 parent 取得老的 Task
+                    self._tasks_queue.append(new_task)
 
                 else:
-                    # generator 自身依旧等待完成
-                    self._futures_queue.append(running_future)
+                    # generator 自身依旧等待完成, 重新加入队列
+                    self._tasks_queue.append(running_task)
 
-        # 当 Future 队列全部运行完毕后, 可以取得结果
-        return future.result()
+        # 当 Task 队列全部运行完毕后, 可以取得结果
+        return task.result()
 
 
 _running_loop = EventLoop()
@@ -157,15 +195,12 @@ def get_running_loop() -> EventLoop:
     return _running_loop
 
 
-def run[T](future: Future[T]) -> T:
-    return get_running_loop().run_until_complete(future)
+def run[T](task: Task[T]) -> T:
+    return get_running_loop().run_until_complete(task)
 
 
-@awaitable
-def gen_sleep(timeout: int) -> Generator[None, None, None]:
-    start_time = time()
+class _Missing: ...
 
-    while time() - start_time < timeout:
-        yield
 
-    return
+class InvalidStateError(Exception):
+    """The operation is not allowed in this state."""
--- from-generator-to-coroutine/test.py
+++ from-generator-to-coroutine/test.py
@@ -2,7 +2,17 @@ from collections.abc import Generator
 from time import time
 from typing import Any
 
-from gen_co import awaitable, gen_sleep, run
+from gen_co import awaitable, run
+
+
+@awaitable
+def gen_sleep(timeout: int) -> Generator[None, None, None]:
+    start_time = time()
+
+    while time() - start_time < timeout:
+        yield
+
+    return
 
 
 @awaitable
@@ -12,11 +22,24 @@ def hello(name: str) -> Generator[Any, None, str]:
 
 
 @awaitable
+def trigger() -> Generator[None, None, None]:
+    yield
+    raise Exception("TriggerError")
+
+
+@awaitable
 def main() -> Generator[Any, Any, None]:
+    try:
+        yield trigger()
+    except Exception as exc:
+        print(f"Got Error: {exc}, ignored!")
+
     print("Current:", time())
 
     # 模拟协程最大的缺陷:
     # 类型推导不看 yield 后面的 hello() 调用, 而是 main() 的返回类型
+    # Generator[Any, Any, None]
+    #                ^^^
     result: str = yield hello("world")
 
     print("Current:", time(), result)
